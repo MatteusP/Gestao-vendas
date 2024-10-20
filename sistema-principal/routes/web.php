c2f81425eb1112194fe4e7462238e480faf91767
Mateus Paiva <matteusp@alu.ufc.br>
2024-10-20 19:46:59 -0300

    Rotas para sistema

--- sistema-principal/routes/web.php
+++ sistema-principal/routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\CouponsController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
+use Illuminate\Contracts\Cache\Store;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +19,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return view('welcome');
+});
+
+
+Route::middleware('auth')->group(function () {
+    
+
+    Route::view('/register', 'auth.register')->name('register');
+
+    //rota para dashboard
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+
+
+    //rotas para usuarios
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/show/{user}', [UserController::class, 'show'])->name('user.show');
+    Route::get('/users/edit/{user}', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/users/delete/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+
+    //rotas para cupons
+    Route::post('/apply-coupon', [CouponsController::class, 'applyCoupon'])->name('apply.coupons');
+
+    //rotas para produtos
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('products/show/{product}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/edit/{product}', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/products/delete/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+    //rotas para vendas
+    Route::post('/sales/store', [SalesController::class, 'store'])->name('sales.store');
+
+    
+});
+
+Route::get('/home', function () {
+    return view('Users');
+});
+
+
 
 require __DIR__.'/auth.php';
